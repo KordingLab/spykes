0bd6c724de7ce6f36b4f99274e6956e388ba4ebc
hugoguh <hugoguh@gmail.com>
2016-06-16 15:35:42 -0500

    Pylint conv changes

--- spykes/spykes.py
+++ spykes/spykes.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Spyke:
+class Spyke(object):
     """
     This class implements several conveniences for
     visualizing firing activity of single neurons
@@ -33,7 +33,7 @@ class Spyke:
         self.firingrate = (n_spikes/n_seconds)
 
     #-----------------------------------------------------------------------
-    def get_raster(self, events, features=[], conditions=[], \
+    def get_raster(self, events, features=None, conditions=None, \
                    window=[-100, 500], binsize=10, plot=True, sort=True):
         """
         Compute the raster and plot it
@@ -47,24 +47,28 @@ class Spyke:
 
         window = [np.floor(window[0]/binsize)*binsize, np.ceil(window[1]/binsize)*binsize]
         # Get a set of binary indicators for trials of interest
+        if features is None:
+            features = list()
+        if conditions is None:
+            conditions = list()
         if len(conditions) > 0:
             trials = self.get_trialtype(features, conditions)
         else:
             trials = np.transpose(np.atleast_2d(np.ones(np.size(events)) == 1))
 
         # Initialize rasters
-        Rasters = dict()
+        rasters = dict()
 
         # Assign time bins
         firstspike = self.spiketimes[0]
         lastspike = self.spiketimes[-1]
-        bins = np.arange(np.floor(firstspike),np.ceil(lastspike), 1e-3*binsize)
+        bins = np.arange(np.floor(firstspike), np.ceil(lastspike), 1e-3*binsize)
 
         # Loop over each raster
-        for r in np.arange(trials.shape[1]):
+        for rast in np.arange(trials.shape[1]):
 
             # Select events relevant to this raster
-            selected_events = events[trials[:,r]]
+            selected_events = events[trials[:, rast]]
 
             # Eliminate events before the first spike after last spike
             selected_events = selected_events[selected_events > firstspike]
@@ -75,40 +79,37 @@ class Spyke:
 
             # bin the events into time bins
             event_counts = np.histogram(selected_events, bins)[0]
-            event_bins =  np.where(event_counts > 0)[0]
+            event_bins = np.where(event_counts > 0)[0]
 
             raster = np.array([(spike_counts[(i+window[0]/binsize): \
                                              (i+window[1]/binsize+1)]) \
                                for i in event_bins])
-            Rasters[r] = raster
+            rasters[rast] = raster
 
         # Show the raster
-        if plot == True:
+        if plot is True:
 
             xtics = [window[0], 0, window[1]]
             xtics = [str(i) for i in xtics]
-            xtics_loc = [0,(-window[0])/binsize,(window[1]-window[0])/binsize]
+            xtics_loc = [0, (-window[0])/binsize, (window[1]-window[0])/binsize]
 
-            for i,r in enumerate(Rasters):
+            for i, rast in enumerate(rasters):
 
-                raster = Rasters[r]
+                raster = rasters[rast]
 
-                if sort == True:
+                if sort is True:
                     # Sorting by total spike count in the duration
                     raster_sorted = raster[np.sum(raster, axis=1).argsort()]
                 else:
                     raster_sorted = raster
 
-                plt.imshow(raster_sorted, aspect='auto', interpolation='none', cmap=plt.get_cmap('Greys'))
+                plt.imshow(raster_sorted, aspect='auto', \
+                    interpolation='none', cmap=plt.get_cmap('Greys'))
                 plt.axvline((-window[0])/binsize, color='r', linestyle='--')
-
-
                 plt.ylabel('trials')
                 plt.xlabel('time [ms]')
-
-                #plt.xticks(np.arange(0,(window[1]-window[0])/binsize,10), xtics)
-                plt.xticks(xtics_loc,xtics)
-                if len(conditions)>0:
+                plt.xticks(xtics_loc, xtics)
+                if len(conditions) > 0:
                     plt.title('neuron %s: Condition %d' % (self.name, i+1))
                 else:
                     plt.title('neuron %s' % self.name)
@@ -123,19 +124,18 @@ class Spyke:
 
                 plt.tick_params(axis='x', which='both', top='off')
                 plt.tick_params(axis='y', which='both', right='off')
-                
                 plt.show()
 
-                if len(conditions)>0:
+                if len(conditions) > 0:
                     print 'Condition %d: %s' % (i+1, str(conditions[i]))
 
         # Return all the rasters
-        return Rasters
+        return rasters
 
     #-----------------------------------------------------------------------
-    def get_psth(self, events, features=[], conditions=[], \
+    def get_psth(self, events, features=None, conditions=None, \
                  window=[-100, 500], binsize=10, plot=True, \
-                 colors=['#F5A21E','#134B64','#EF3E34','#02A68E','#FF07CD']):
+                 colors=['#F5A21E', '#134B64', '#EF3E34', '#02A68E', '#FF07CD']):
         """
         Compute the psth and plot it
 
@@ -145,57 +145,62 @@ class Spyke:
                 (e.g. stimulus/ trial/ fixation onset, etc.)
         """
 
+        if features is None:
+            features = []
+
+        if conditions is None:
+            conditions = []
+
         window = [np.floor(window[0]/binsize)*binsize, np.ceil(window[1]/binsize)*binsize]
         # Get all the rasters first
-        Rasters = self.get_raster(events, features, conditions, window, binsize, plot=False)
+        rasters = self.get_raster(events, features, conditions, window, binsize, plot=False)
 
         # Open the figure
         # Initialize PSTH
-        PSTH = dict()
+        psth = dict()
 
         # Compute the PSTH
-        for i, r in enumerate(Rasters):
+        for i, rast in enumerate(rasters):
 
-            PSTH[i] = dict()
-            raster = Rasters[r]
-            mean_psth = np.mean(raster,axis=0)/(1e-3*binsize)
-            std_psth = np.sqrt(np.var(raster,axis=0))/(1e-3*binsize)
+            psth[i] = dict()
+            raster = rasters[rast]
+            mean_psth = np.mean(raster, axis=0)/(1e-3*binsize)
+            std_psth = np.sqrt(np.var(raster, axis=0))/(1e-3*binsize)
 
             sem_psth = std_psth/np.sqrt(float(np.shape(raster)[0]))
 
-            PSTH[i]['mean'] = mean_psth
-            PSTH[i]['sem'] = sem_psth
+            psth[i]['mean'] = mean_psth
+            psth[i]['sem'] = sem_psth
 
         # Visualize the PSTH
-        if plot == True:
+        if plot is True:
 
-            fig = plt.figure()
+            plt.figure()
 
             scale = 0.1
             y_min = (1.0-scale)*np.min([np.min( \
-                np.mean(Rasters[raster_idx],axis=0)/(1e-3*binsize)) \
-                for raster_idx in Rasters])
+                np.mean(rasters[raster_idx], axis=0)/(1e-3*binsize)) \
+                for raster_idx in rasters])
             y_max = (1.0+scale)*np.max([np.max( \
-                np.mean(Rasters[raster_idx],axis=0)/(1e-3*binsize)) \
-                for raster_idx in Rasters])
+                np.mean(rasters[raster_idx], axis=0)/(1e-3*binsize)) \
+                for raster_idx in rasters])
 
             legend = ['event onset']
 
-            xx = np.append( \
+            time_bins = np.append( \
                 np.linspace(window[0], 0, num=np.abs(window[0])/binsize+1), \
                 np.linspace(0, window[1], num=np.abs(window[1])/binsize+1)[1:])
 
-            plt.plot([0,0],[y_min,y_max], color='k', ls = '--')
+            plt.plot([0, 0], [y_min, y_max], color='k', ls='--')
 
-            for i, r in enumerate(Rasters):
+            for i, rast in enumerate(rasters):
+                plt.plot(time_bins, psth[i]['mean'], color=colors[i], lw=1.5)
 
-                plt.plot(xx, PSTH[i]['mean'], color=colors[i], lw=1.5)
+            for i, rast in enumerate(rasters):
+                plt.fill_between(time_bins, psth[i]['mean']-psth[i]['sem'], \
+                    psth[i]['mean']+psth[i]['sem'], color=colors[i], alpha=0.2)
 
-            for i, r in enumerate(Rasters):
-                
-                plt.fill_between(xx, PSTH[i]['mean']-PSTH[i]['sem'], PSTH[i]['mean']+PSTH[i]['sem'], color=colors[i], alpha=0.2)
-
-                if len(conditions)>0:
+                if len(conditions) > 0:
                     legend.append('Condition %d' % (i+1))
                 else:
                     legend.append('all')
@@ -212,16 +217,17 @@ class Spyke:
             ax.get_xaxis().set_tick_params(direction='out')
 
             plt.tick_params(axis='y', right='off')
-            plt.tick_params(axis='x', top='off')  
+            plt.tick_params(axis='x', top='off')
 
             plt.legend(legend, frameon=False)
 
             plt.show()
 
         for i, cond in enumerate(conditions):
-            print 'Condition %d: %s; %d trials' % (cond+1,str(conditions[cond]),np.shape(Rasters[i])[0])
+            print 'Condition %d: %s; %d trials' % \
+                (cond+1, str(conditions[cond]), np.shape(rasters[i])[0])
 
-        return PSTH
+        return psth
 
     #-----------------------------------------------------------------------
     def get_trialtype(self, features, conditions):
@@ -241,15 +247,15 @@ class Spyke:
         trials: bool, n x 1 array of indicators
         """
         trials = []
-        for r in conditions:
-            condition = conditions[r]
-            trials.append([np.all([np.all((features[r] >= condition[r][0], \
-                                 features[r] <= condition[r][-1]), axis=0) \
-                                 for r in condition], axis=0)])
+        for rast in conditions:
+            condition = conditions[rast]
+            trials.append([np.all([np.all((features[rast] >= condition[rast][0], \
+                                 features[rast] <= condition[rast][-1]), axis=0) \
+                                 for rast in condition], axis=0)])
         return np.transpose(np.atleast_2d(np.squeeze(trials)))
 
     #-----------------------------------------------------------------------
-    def get_spikecounts(self, events, window = 1e-3*np.array([50.0, 100.0])):
+    def get_spikecounts(self, events, window=1e-3*np.array([50.0, 100.0])):
         """
         Parameters
         ----------
@@ -263,8 +269,8 @@ class Spyke:
         """
         spiketimes = self.spiketimes
         spikecounts = np.zeros(events.shape)
-        for e in range(events.shape[0]):
-            spikecounts[e] = np.sum(np.all((spiketimes >= events[e] + window[0],\
-                                            spiketimes <= events[e] + window[1]),\
+        for eve in range(events.shape[0]):
+            spikecounts[eve] = np.sum(np.all((spiketimes >= events[eve] + window[0],\
+                                            spiketimes <= events[eve] + window[1]),\
                                             axis=0))
         return spikecounts
