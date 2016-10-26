6f0883f37da4fbf3556d7e494688a5beafe1549b
timshell <magrawa1@swarthmore.edu>
2016-10-26 21:26:55 +0100

    increasing coverage by testing plots

--- tests/test_neurovis.py
+++ tests/test_neurovis.py
@@ -1,11 +1,12 @@
 from spykes.neurovis import NeuroVis
-from nose.tools import assert_true, assert_equal, assert_raises
+from nose.tools import assert_true, assert_equal
 import numpy as np
 import pandas as pd
+from mock import patch
 
 
-
-def test_neurovis():
+@patch('matplotlib.pyplot.show')
+def test_neurovis(mock_show):
 
     num_spikes = 500
     num_trials = 10
@@ -13,7 +14,7 @@ def test_neurovis():
     binsize = 100
     window = [-500, 1500]
 
-    rand_spiketimes = np.sort(num_trials*np.random.rand(num_spikes))
+    rand_spiketimes = np.sort(num_trials * np.random.rand(num_spikes))
 
     neuron = NeuroVis(spiketimes=rand_spiketimes)
 
@@ -23,21 +24,21 @@ def test_neurovis():
     condition_num = 'responseNum'
     condition_bool = 'responseBool'
 
-    start_times = rand_spiketimes[0::num_spikes/num_trials]
+    start_times = rand_spiketimes[0::num_spikes / num_trials]
     df['trialStart'] = start_times
 
-    df[event] = df['trialStart']+ np.random.rand(num_trials)
+    df[event] = df['trialStart'] + np.random.rand(num_trials)
 
-    event_times = ((start_times[:-1] + start_times[1:])/2).tolist()
-    event_times.append(start_times[-1]+ np.random.rand())
+    event_times = ((start_times[:-1] + start_times[1:]) / 2).tolist()
+    event_times.append(start_times[-1] + np.random.rand())
 
     df[event] = event_times
 
     df[condition_num] = np.random.rand(num_trials)
     df[condition_bool] = df[condition_num] < 0.5
 
-    raster = neuron.get_raster(event=event, conditions=condition_bool, df=df, 
-        plot=False, binsize=binsize, window=window)
+    raster = neuron.get_raster(event=event, conditions=condition_bool, df=df,
+                               plot=True, binsize=binsize, window=window)
 
     assert_equal(raster['event'], event)
     assert_equal(raster['conditions'], condition_bool)
@@ -49,14 +50,14 @@ def test_neurovis():
     for cond_id in raster['data'].keys():
 
         assert_true(cond_id in df[condition_bool])
-        assert_equal(raster['data'][cond_id].shape[1], 
-            (window[1]-window[0])/binsize)
+        assert_equal(raster['data'][cond_id].shape[1],
+                     (window[1] - window[0]) / binsize)
         total_trials += raster['data'][cond_id].shape[0]
 
     assert_equal(total_trials, num_trials)
 
-    psth = neuron.get_psth(event=event, conditions=condition_bool, df=df, 
-        plot=False, binsize=binsize, window=window)
+    psth = neuron.get_psth(event=event, conditions=condition_bool, df=df,
+                           plot=True, binsize=binsize, window=window)
 
     assert_equal(psth['window'], window)
     assert_equal(psth['binsize'], binsize)
@@ -66,15 +67,9 @@ def test_neurovis():
     for cond_id in psth['data'].keys():
 
         assert_true(cond_id in df[condition_bool])
-        assert_equal(psth['data'][cond_id]['mean'].shape[0], 
-            (window[1]-window[0])/binsize)
-        assert_equal(psth['data'][cond_id]['sem'].shape[0], 
-            (window[1]-window[0])/binsize)
+        assert_equal(psth['data'][cond_id]['mean'].shape[0],
+                     (window[1] - window[0]) / binsize)
+        assert_equal(psth['data'][cond_id]['sem'].shape[0],
+                     (window[1] - window[0]) / binsize)
 
-    spikecounts = neuron.get_spikecounts(event=event, df=df, 
-        window=[0,num_trials])
-
-
-
-
-
+    neuron.get_spikecounts(event=event, df=df, window=[0, num_trials])
