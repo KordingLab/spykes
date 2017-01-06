2651fdd04ae33e47e25060bb6203a885f1d8ff08
pavanramkumar <pavan.ramkumar@gmail.com>
2017-01-06 15:17:33 -0600

    Make install

--- spykes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .neurovis import NeuroVis
-from .neuropop import NeuroPop
-from .popvis import PopVis
-from .strf import STRF
-from .utils import slow_exp, grad_slow_exp, log_likelihood, circ_corr
-__version__ = '0.1.dev'
