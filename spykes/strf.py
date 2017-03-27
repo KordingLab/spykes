ac2f826ebc00ca81e9969ccfd408f2ac66447fe3
Pavan Ramkumar <pavan.ramkumar@gmail.com>
2017-03-27 16:37:00 -0400

    remove print

--- spykes/strf.py
+++ spykes/strf.py
@@ -104,7 +104,6 @@ class STRF(object):
 
         for y in range(n_tiles.astype(int)):
             for x in range(n_tiles.astype(int)):
-                print y, x
                 gaussian_mask = self.make_2d_gaussian(center=(centers[x],
                                                               centers[y]))
                 spatial_basis.append(gaussian_mask)
