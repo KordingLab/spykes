64f8eef26a2d30533e427bbe7d8b81c4acbc7d71
timshell <magrawa1@swarthmore.edu>
2017-03-28 12:00:57 -0400

    camel case to underscores

--- examples/plot_neuropixels_example.py
+++ examples/plot_neuropixels_example.py
@@ -36,117 +36,118 @@ plt.style.use('seaborn-ticks')
 # 1 Read In Data
 # -----------------------------
 
-folderNames = ['posterior', 'frontal']
+folder_names = ['posterior', 'frontal']
 Fs = 30000.0
 
 striatum = list()
-motorCtx = list()
+motor_ctx = list()
 thalamus = list()
 hippocampus = list()
-visualCtx = list()
+visual_ctx = list()
 
 # a lot of this code is adapted from Cortex Lab's MATLAB script
 # see here: http://data.cortexlab.net/dualPhase3/data/script_dualPhase3.m
 
-for name in folderNames:
+for name in folder_names:
 
     clusters = np.squeeze(np.load(name + '/spike_clusters.npy'))
-    spikeTimes = (np.squeeze(np.load(name + '/spike_times.npy'))) / Fs
-    spikeTemplates = (np.squeeze(np.load(name + '/spike_templates.npy')))
+    spike_times = (np.squeeze(np.load(name + '/spike_times.npy'))) / Fs
+    spike_templates = (np.squeeze(np.load(name + '/spike_templates.npy')))
 
     temps = (np.squeeze(np.load(name + '/templates.npy')))
     winv = (np.squeeze(np.load(name + '/whitening_mat_inv.npy')))
-    yCoords = (np.squeeze(np.load(name + '/channel_positions.npy')))[:, 1]
+    y_coords = (np.squeeze(np.load(name + '/channel_positions.npy')))[:, 1]
 
     # frontal times need to align with posterior
     if (name == 'frontal'):
-        timeCorrection = np.load('timeCorrection.npy')
-        spikeTimes *= timeCorrection[0]
-        spikeTimes += timeCorrection[1]
+        time_correction = np.load('time_correction.npy')
+        spike_times *= time_correction[0]
+        spike_times += time_correction[1]
 
     data = np.recfromcsv(name + '/cluster_groups.csv', delimiter='\t')
     cids = np.array([x[0] for x in data])
     cfg = np.array([x[1] for x in data])
 
     # find good clusters and only use those spikes
-    goodClusters = cids[cfg == 'good']
-    goodIndices = (np.in1d(clusters, goodClusters))
+    good_clusters = cids[cfg == 'good']
+    good_indices = (np.in1d(clusters, good_clusters))
 
-    realSpikes = spikeTimes[goodIndices]
-    realClusters = clusters[goodIndices]
-    realSpikeTemplates = spikeTemplates[goodIndices]
+    real_spikes = spike_times[good_indices]
+    real_clusters = clusters[good_indices]
+    real_spike_templates = spike_templates[good_indices]
 
     # find how many spikes per cluster and then order spikes by which cluster
     # they are in
 
-    countsPerCluster = np.bincount(realClusters)
+    counts_per_cluster = np.bincount(real_clusters)
 
-    sort_idx = np.argsort(realClusters)
-    sortedClusters = realClusters[sort_idx]
-    sortedSpikes = realSpikes[sort_idx]
-    sortedSpikeTemplates = realSpikeTemplates[sort_idx]
+    sort_idx = np.argsort(real_clusters)
+    sorted_clusters = real_clusters[sort_idx]
+    sorted_spikes = real_spikes[sort_idx]
+    sorted_spike_templates = real_spike_templates[sort_idx]
 
     # find depth for each spike
     # this is translated from Cortex Lab's MATLAB code
     # for more details, check out the original code here:
     # https://github.com/cortex-lab/spikes/blob/master/analysis/templatePositionsAmplitudes.m
 
-    tempsUnW = np.zeros(temps.shape)
+    temps_unw = np.zeros(temps.shape)
     for t in range(temps.shape[0]):
-        tempsUnW[t, :, :] = np.dot(temps[t, :, :], winv)
+        temps_unw[t, :, :] = np.dot(temps[t, :, :], winv)
 
-    tempChanAmps = np.max(tempsUnW, axis=1) - np.min(tempsUnW, axis=1)
-    tempsAmps = np.max(tempChanAmps, axis=1)
-    threshVals = tempsAmps * 0.3
+    temp_chan_amps = np.ptp(temps_unw, axis=1)
+    temps_amps = np.max(temp_chan_amps, axis=1)
+    thresh_vals = temps_amps * 0.3
 
-    threshVals = [threshVals for i in range(tempChanAmps.shape[1])]
-    threshVals = np.stack(threshVals, axis=1)
+    thresh_vals = [thresh_vals for i in range(temp_chan_amps.shape[1])]
+    thresh_vals = np.stack(thresh_vals, axis=1)
 
-    tempChanAmps[tempChanAmps < threshVals] = 0
+    temp_chan_amps[temp_chan_amps < thresh_vals] = 0
 
-    yCoords = np.reshape(yCoords, (yCoords.shape[0], 1))
-    tempDepths = np.sum(
-        np.dot(tempChanAmps, yCoords), axis=1) / (np.sum(tempChanAmps, axis=1))
+    y_coords = np.reshape(y_coords, (y_coords.shape[0], 1))
+    temp_depths = np.sum(
+        np.dot(temp_chan_amps, y_coords), axis=1) / (np.sum(temp_chan_amps,
+                                                     axis=1))
 
-    sortedSpikeDepths = tempDepths[sortedSpikeTemplates]
+    sorted_spike_depths = temp_depths[sorted_spike_templates]
 
     # create neurons and find region
 
     accumulator = 0
 
-    for i, count in enumerate(countsPerCluster):
+    for idx, count in enumerate(counts_per_cluster):
 
         if count > 0:
 
-            spiketimes = sortedSpikes[accumulator:accumulator + count]
-            neuron = NeuroVis(spiketimes=spiketimes, name='%d' % (i))
-            clusterDepth = np.mean(
-                sortedSpikeDepths[accumulator:accumulator + count])
+            spike_times = sorted_spikes[accumulator:accumulator + count]
+            neuron = NeuroVis(spike_times=spike_times, name='%d' % (idx))
+            cluster_depth = np.mean(
+                sorted_spike_depths[accumulator:accumulator + count])
 
             if name == 'frontal':
 
-                if (clusterDepth > 0 and clusterDepth < 1550):
+                if (cluster_depth > 0 and cluster_depth < 1550):
                     striatum.append(neuron)
-                elif (clusterDepth > 1550 and clusterDepth < 3840):
-                    motorCtx.append(neuron)
+                elif (cluster_depth > 1550 and cluster_depth < 3840):
+                    motor_ctx.append(neuron)
 
             elif name == 'posterior':
 
-                if (clusterDepth > 0 and clusterDepth < 1634):
+                if (cluster_depth > 0 and cluster_depth < 1634):
                     thalamus.append(neuron)
-                elif (clusterDepth > 1634 and clusterDepth < 2797):
+                elif (cluster_depth > 1634 and cluster_depth < 2797):
                     hippocampus.append(neuron)
-                elif (clusterDepth > 2797 and clusterDepth < 3840):
-                    visualCtx.append(neuron)
+                elif (cluster_depth > 2797 and cluster_depth < 3840):
+                    visual_ctx.append(neuron)
 
             accumulator += count
 
 
 print("Striatum (n = %d)" % len(striatum))
-print("Motor Cortex (n = %d)" % len(motorCtx))
+print("Motor Cortex (n = %d)" % len(motor_ctx))
 print("Thalamus (n = %d)" % len(thalamus))
 print("Hippocampus (n = %d)" % len(hippocampus))
-print("Visual Cortex (n = %d)" % len(visualCtx))
+print("Visual Cortex (n = %d)" % len(visual_ctx))
 
 ########################################################
 # 2 Create Data Frame
@@ -154,11 +155,11 @@ print("Visual Cortex (n = %d)" % len(visualCtx))
 
 df = pd.DataFrame()
 
-rawData = scipy.io.loadmat('experiment1stimInfo.mat')
+raw_data = scipy.io.loadmat('experiment1stimInfo.mat')
 
-df['start'] = np.squeeze(rawData['stimStarts'])
-df['stop'] = np.squeeze(rawData['stimStops'])
-df['stimulus'] = np.squeeze(rawData['stimIDs'])
+df['start'] = np.squeeze(raw_data['stimStarts'])
+df['stop'] = np.squeeze(raw_data['stimStops'])
+df['stimulus'] = np.squeeze(raw_data['stimIDs'])
 
 print(df.head())
 
@@ -187,7 +188,7 @@ pop.plot_population_psth(all_psth=all_psth, cond_id=[1, 7, 12])
 # 3.2 Frontal
 # ~~~~~~~~~~~~
 
-pop = PopVis(striatum + motorCtx, name='Frontal')
+pop = PopVis(striatum + motor_ctx, name='Frontal')
 
 fig = plt.figure(figsize=(30, 20))
 
@@ -206,7 +207,7 @@ pop.plot_population_psth(all_psth=all_psth, cond_id=[1, 7, 12])
 # 3.3 All Neurons
 # ~~~~~~~~~~~~
 
-pop = PopVis(striatum + motorCtx + thalamus + hippocampus + visualCtx)
+pop = PopVis(striatum + motor_ctx + thalamus + hippocampus + visual_ctx)
 
 fig = plt.figure(figsize=(30, 20))
 
@@ -225,20 +226,20 @@ pop.plot_population_psth(all_psth=all_psth, cond_id=[1, 7, 12])
 # 3.4 Striatum vs. Motor Cortex
 # ~~~~~~~~~~~~
 
-striatumPop = PopVis(striatum, name='Striatum')
-motorCtxPop = PopVis(motorCtx, name='Motor Cortex')
+striatum_pop = PopVis(striatum, name='Striatum')
+motor_ctx_pop = PopVis(motor_ctx, name='Motor Cortex')
 
-striatum_psth = striatumPop.get_all_psth(
+striatum_psth = striatum_pop.get_all_psth(
     event='start', df=df, conditions='stimulus', plot=False, binsize=100,
     window=[-500, 2000])
-motorCtx_psth = motorCtxPop.get_all_psth(
+motor_ctx_psth = motor_ctx_pop.get_all_psth(
     event='start', df=df, conditions='stimulus', plot=False, binsize=100,
     window=[-500, 2000])
 
 ########################################################
 
-striatumPop.plot_population_psth(all_psth=striatum_psth, cond_id=[1, 7, 12])
+striatum_pop.plot_population_psth(all_psth=striatum_psth, cond_id=[1, 7, 12])
 
 ########################################################
 
-motorCtxPop.plot_population_psth(all_psth=motorCtx_psth, cond_id=[1, 7, 12])
+motor_ctx_pop.plot_population_psth(all_psth=motor_ctx_psth, cond_id=[1, 7, 12])
