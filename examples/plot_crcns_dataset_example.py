ee9e0b212726d9b3c6bad79f011e51a06d8b4cd4
timshell <magrawa1@swarthmore.edu>
2017-03-28 11:35:48 -0400

    pep8 change

--- examples/plot_crcns_dataset_example.py
+++ examples/plot_crcns_dataset_example.py
@@ -1,22 +1,26 @@
 """
-============================================
+=====================
 CRCNS DataSet Example
-============================================
+=====================
 
 A demonstration of Spykes's functionality to reproduce a figure from Li et al's
 "A motor cortex circuit for motor planning and movement."
 
 """
+# Authors: Eric Larson <larson.eric.d@gmail.com>
+#
+# License: BSD (3-clause)
 
 ########################################################
-
-import matplotlib.pyplot as plt
-plt.style.use('seaborn-ticks')
-plt.switch_backend('Agg')
 
 import numpy as np
 import pandas as pd
 from spykes.neurovis import NeuroVis
+import matplotlib.pyplot as plt
+import scipy.io
+
+plt.style.use('seaborn-ticks')
+plt.switch_backend('Agg')
 
 ########################################################
 # 0 Overview: Reproduce Figure
@@ -25,12 +29,14 @@ from spykes.neurovis import NeuroVis
 # 0.1 Article
 # ~~~~~~~~~~~~~
 #
-# Li, Nuo, et al. "A motor cortex circuit for motor planning and movement." 
+# Li, Nuo, et al. "A motor cortex circuit for motor planning and movement."
 # Nature 519.7541 (2015): 51-56.
 # [`link to
-# paper <http://www.nature.com/nature/journal/v519/n7541/full/nature14178.html>`__].
+# paper
+# <http://www.nature.com/nature/journal/v519/n7541/full/nature14178.html>`__].
 # We aim to reproduce
-# `this figure <http://www.nature.com/nature/journal/v519/n7541/fig_tab/nature14178_F2.html>`__
+# `this figure
+# <http://www.nature.com/nature/journal/v519/n7541/fig_tab/nature14178_F2.html>`__
 #
 #
 # 0.2 Dataset
@@ -49,16 +55,17 @@ from spykes.neurovis import NeuroVis
 # 1.1 Download Data
 # ~~~~~~~~~~~~~~~~~
 #
-# Register in ``CRCNS`` [`link to request account <https://crcns.org/request-account>`__]
+# Register in ``CRCNS`` [`link to request account
+# <https://crcns.org/request-account>`__]
 #
-# Download file ``data_structure_ANM218457.tar.gz`` [`link <https://portal.nersc.gov/project/crcns/download/alm-1/datafiles/data_structure_files>`__]
+# Download file ``data_structure_ANM218457.tar.gz`` [`link
+# <https://portal.nersc.gov/project/crcns/download/alm-1/datafiles/data_structure_files>`__]
 #
 # Unzip it and you will find file ``data_structure_ANM218457_20131006.mat``
 #
 # 1.2 Load Data
 # ~~~~~~~~~~~~~
 
-import scipy.io
 mat = scipy.io.loadmat('data_structure_ANM218457_20131006.mat')
 
 ########################################################
@@ -67,12 +74,14 @@ mat = scipy.io.loadmat('data_structure_ANM218457_20131006.mat')
 # --------------------
 
 neuron_n = 9
-spike_times = mat['obj']['eventSeriesHash'][0][0]['value'][0][0][0][neuron_n-1][0][0][1]
+spike_times = mat['obj']['eventSeriesHash'][0][
+    0]['value'][0][0][0][neuron_n - 1][0][0][1]
 spike_times = [i[0] for i in spike_times]
 
 # instantiate neuron
 neuron = NeuroVis(spike_times, neuron_n)
-print('neuron %d has a firing rate of %0.2f spikes per second' % (neuron_n, neuron.firingrate))
+print('neuron %d has a firing rate of %0.2f spikes per second' %
+      (neuron_n, neuron.firingrate))
 
 
 ########################################################
@@ -91,7 +100,8 @@ print('neuron %d has a firing rate of %0.2f spikes per second' % (neuron_n, neur
 
 data_df = pd.DataFrame()
 data_df['trialStart'] = mat['obj']['trialStartTimes'][0][0][0]
-data_df['cueTimes'] = np.squeeze(mat['obj']['trialPropertiesHash'][0][0][0][0][2][0][2])
+data_df['cueTimes'] = np.squeeze(
+    mat['obj']['trialPropertiesHash'][0][0][0][0][2][0][2])
 data_df['RealCueTimes'] = data_df['trialStart'] + data_df['cueTimes']
 
 # Collect all the events and display them
@@ -105,15 +115,18 @@ data_df[events].head()
 
 
 trialTypeMat = mat['obj']['trialTypeMat'][0][0].astype(np.bool_)
-trialTypeStr = np.squeeze(np.stack(np.squeeze(mat['obj']['trialTypeStr'][0][0])))
+trialTypeStr = np.squeeze(
+    np.stack(np.squeeze(mat['obj']['trialTypeStr'][0][0])))
 
 for ind, feat in enumerate(trialTypeStr):
     data_df[str(feat)] = trialTypeMat[ind]
 
-data_df['GoodTrials'] = np.squeeze(mat['obj']['trialPropertiesHash'][0][0][0][0][2][0][3]).astype(np.bool_)
+data_df['GoodTrials'] = np.squeeze(
+    mat['obj']['trialPropertiesHash'][0][0][0][0][2][0][3]).astype(np.bool_)
 
 # Collect all features and display them
-features = ['HitR', 'HitL', 'ErrR', 'ErrL', 'NoLickR', 'NoLickL', 'LickEarly', 'StimTrials', 'GoodTrials']
+features = ['HitR', 'HitL', 'ErrR', 'ErrL', 'NoLickR',
+            'NoLickL', 'LickEarly', 'StimTrials', 'GoodTrials']
 data_df[features].head()
 
 ########################################################
@@ -123,13 +136,16 @@ data_df[features].head()
 
 ########################################################
 
-features_of_interst = ['HitR','HitL', 'ErrR', 'ErrL']
+features_of_interst = ['HitR', 'HitL', 'ErrR', 'ErrL']
 
-data_df['response'] = data_df[features_of_interst].apply(lambda row: row.argmax() if row.max()==True else '',axis=1)
-data_df['correct'] = data_df['response'].map(lambda s: {'Hit':True,'Err':False,'':np.nan}[s[:3]])
-data_df['response'] = data_df['response'].map(lambda s: {'L':'Lick left','R':'Lick right','':''}[s[-1:]])
+data_df['response'] = data_df[features_of_interst].apply(
+    lambda row: row.argmax() if row.max() else '', axis=1)
+data_df['correct'] = data_df['response'].map(
+    lambda s: {'Hit': True, 'Err': False, '': np.nan}[s[:3]])
+data_df['response'] = data_df['response'].map(
+    lambda s: {'L': 'Lick left', 'R': 'Lick right', '': ''}[s[-1:]])
 
-data_df[['HitR','HitL', 'ErrR', 'ErrL','correct','response']].head()
+data_df[['HitR', 'HitL', 'ErrR', 'ErrL', 'correct', 'response']].head()
 
 ########################################################
 #
@@ -139,10 +155,8 @@ data_df[['HitR','HitL', 'ErrR', 'ErrL','correct','response']].head()
 ########################################################
 
 # isolate trials of interest
-trials_df = data_df[(
-                    (data_df['GoodTrials']==True) &
-                    (data_df['correct']==True)
-                    )]
+trials_df = data_df[((data_df['GoodTrials'] == True) &
+                    (data_df['correct'] == True))]
 
 ########################################################
 #
@@ -153,8 +167,8 @@ trials_df = data_df[(
 # ~~~~~~~~~~~~~~~~~~~
 
 event = 'RealCueTimes'
-conditions='response'
-window = [-3000,2000]
+conditions = 'response'
+window = [-3000, 2000]
 
 rasters_fig2b1 = neuron.get_raster(event=event,
                                    conditions=conditions,
@@ -168,12 +182,12 @@ rasters_fig2b1 = neuron.get_raster(event=event,
 # 6.2 PSTH
 # ~~~~~~~~~~~~~~~~~~~
 
-plt.figure(figsize=(8,5))
+plt.figure(figsize=(8, 5))
 neuron.get_psth(event=event,
                 df=trials_df,
                 conditions=conditions,
                 window=window,
-                binsize=100);
+                binsize=100)
 plt.title('')
 plt.show()
 
@@ -188,39 +202,38 @@ cmap = [plt.get_cmap('Blues'), plt.get_cmap('Reds')]
 colors = ['#E82F3A', '#3B439A']
 
 
-#get rasters------------------------------------------------------
+# get rasters------------------------------------------------------
 rasters_fig2b1 = neuron.get_raster(event=event,
                                    conditions=conditions,
                                    df=trials_df,
                                    window=window,
                                    binsize=20,
-                                   plot=False);
+                                   plot=False)
 
 
-
-plt.figure(figsize=(8,10))
+plt.figure(figsize=(8, 10))
 cond_ids = rasters_fig2b1['data'].keys()[::-1]
 
-#plot rasters-------------------------------------------------------
+# plot rasters-------------------------------------------------------
 for i, cond_id in enumerate(cond_ids):
-    plt.subplot(4,1,i+1)
+    plt.subplot(4, 1, i + 1)
     neuron.plot_raster(rasters=rasters_fig2b1,
                        cond_id=cond_id,
                        cmap=cmap[i],
                        sortby=None,
                        has_title=False)
     plt.xlabel('')
-    
-    
-#plot psth-------------------------------------------------------    
+
+
+# plot psth-------------------------------------------------------
 plt.subplot(212)
 psth = neuron.get_psth(event=event,
-                conditions=conditions,
-                df=trials_df,
-                window=window,
-                binsize=100,
-                plot=True,
-                colors=colors);
+                       conditions=conditions,
+                       df=trials_df,
+                       window=window,
+                       binsize=100,
+                       plot=True,
+                       colors=colors)
 plt.title('')
 plt.show()
 
@@ -232,13 +245,13 @@ plt.show()
 
 plt.style.use('ggplot')
 
-plt.figure(figsize=(8,5))
+plt.figure(figsize=(8, 5))
 neuron.get_psth(event=event,
                 conditions=conditions,
                 df=trials_df,
                 event_name='Cue On',
                 window=window,
                 conditions_names=['Lick Left', 'Lick Right'],
-                binsize=100);
+                binsize=100)
 plt.title('')
 plt.show()
--- examples/plot_neuropixels_example.py
+++ examples/plot_neuropixels_example.py
@@ -1,39 +1,37 @@
 """
-============================================
+===================
 Neuropixels Example
-============================================
+===================
 
 Use spykes to analyze data from UCL's Neuropixels
 
 """
+# Authors: Mayank Agrawal <mayankagrawal96@gmail.com>
+#
+# License: MIT
 
 ########################################################
 
-
-import warnings
-warnings.filterwarnings('ignore')
-
-import matplotlib.pyplot as plt
-plt.style.use('seaborn-ticks')
-# plt.switch_backend('Agg')
-
-import csv
 import numpy as np
 import pandas as pd
 import scipy.io
 from spykes.neurovis import NeuroVis
 from spykes.popvis import PopVis
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-ticks')
+plt.switch_backend('Agg')
 
 ########################################################
 # Neuropixels
 # -----------------------------
 # Neuropixels is a new recording technique by UCL's `Cortex Lab
 # <http://www.ucl.ac.uk/neuropixels>`__ that is able to measure data from
-# hundreds of neurons. Below we show how this data can be worked with in Spykes.
+# hundreds of neurons. Below we show how this data can be worked with in Spykes
 #
 # 0 Download Data
 # -----------------------------
-# 
+#
 # Download all data `here <http://data.cortexlab.net/dualPhase3/data/>`__.
 #
 # 1 Read In Data
@@ -52,103 +50,104 @@ visualCtx = list()
 # see here: http://data.cortexlab.net/dualPhase3/data/script_dualPhase3.m
 
 for name in folderNames:
-    
+
     clusters = np.squeeze(np.load(name + '/spike_clusters.npy'))
-    spikeTimes = (np.squeeze(np.load(name + '/spike_times.npy')))/Fs
+    spikeTimes = (np.squeeze(np.load(name + '/spike_times.npy'))) / Fs
     spikeTemplates = (np.squeeze(np.load(name + '/spike_templates.npy')))
-    
+
     temps = (np.squeeze(np.load(name + '/templates.npy')))
     winv = (np.squeeze(np.load(name + '/whitening_mat_inv.npy')))
-    yCoords = (np.squeeze(np.load(name + '/channel_positions.npy')))[:,1]
-    
+    yCoords = (np.squeeze(np.load(name + '/channel_positions.npy')))[:, 1]
+
     # frontal times need to align with posterior
     if (name == 'frontal'):
         timeCorrection = np.load('timeCorrection.npy')
-        spikeTimes *= timeCorrection[0] 
+        spikeTimes *= timeCorrection[0]
         spikeTimes += timeCorrection[1]
-    
-    data = np.recfromcsv(name + '/cluster_groups.csv', delimiter = '\t')
+
+    data = np.recfromcsv(name + '/cluster_groups.csv', delimiter='\t')
     cids = np.array([x[0] for x in data])
     cfg = np.array([x[1] for x in data])
-    
-    
+
     # find good clusters and only use those spikes
     goodClusters = cids[cfg == 'good']
     goodIndices = (np.in1d(clusters, goodClusters))
-        
+
     realSpikes = spikeTimes[goodIndices]
     realClusters = clusters[goodIndices]
     realSpikeTemplates = spikeTemplates[goodIndices]
-    
-    # find how many spikes per cluster and then order spikes by which cluster they are in
-    
+
+    # find how many spikes per cluster and then order spikes by which cluster
+    # they are in
+
     countsPerCluster = np.bincount(realClusters)
-    
-    sort_idx = np.argsort(realClusters) 
+
+    sort_idx = np.argsort(realClusters)
     sortedClusters = realClusters[sort_idx]
     sortedSpikes = realSpikes[sort_idx]
     sortedSpikeTemplates = realSpikeTemplates[sort_idx]
-    
+
     # find depth for each spike
     # this is translated from Cortex Lab's MATLAB code
-    # for more details, check out the original code here: 
+    # for more details, check out the original code here:
     # https://github.com/cortex-lab/spikes/blob/master/analysis/templatePositionsAmplitudes.m
-    
+
     tempsUnW = np.zeros(temps.shape)
     for t in range(temps.shape[0]):
-        tempsUnW[t,:,:] = np.dot(temps[t,:,:],winv)
-        
-    
+        tempsUnW[t, :, :] = np.dot(temps[t, :, :], winv)
+
     tempChanAmps = np.max(tempsUnW, axis=1) - np.min(tempsUnW, axis=1)
     tempsAmps = np.max(tempChanAmps, axis=1)
     threshVals = tempsAmps * 0.3
-    
+
     threshVals = [threshVals for i in range(tempChanAmps.shape[1])]
     threshVals = np.stack(threshVals, axis=1)
-    
+
     tempChanAmps[tempChanAmps < threshVals] = 0
-        
+
     yCoords = np.reshape(yCoords, (yCoords.shape[0], 1))
-    tempDepths = np.sum(np.dot(tempChanAmps,yCoords), axis=1) / (np.sum(tempChanAmps,axis=1))
-    
+    tempDepths = np.sum(
+        np.dot(tempChanAmps, yCoords), axis=1) / (np.sum(tempChanAmps, axis=1))
+
     sortedSpikeDepths = tempDepths[sortedSpikeTemplates]
-    
+
     # create neurons and find region
-                
+
     accumulator = 0
-    
+
     for i, count in enumerate(countsPerCluster):
-        
+
         if count > 0:
-            
-            spiketimes = sortedSpikes[accumulator:accumulator+count]
+
+            spiketimes = sortedSpikes[accumulator:accumulator + count]
             neuron = NeuroVis(spiketimes=spiketimes, name='%d' % (i))
-            clusterDepth = np.mean(sortedSpikeDepths[accumulator:accumulator+count])
-            
+            clusterDepth = np.mean(
+                sortedSpikeDepths[accumulator:accumulator + count])
+
             if name == 'frontal':
-                
+
                 if (clusterDepth > 0 and clusterDepth < 1550):
                     striatum.append(neuron)
                 elif (clusterDepth > 1550 and clusterDepth < 3840):
                     motorCtx.append(neuron)
-            
+
             elif name == 'posterior':
-                
+
                 if (clusterDepth > 0 and clusterDepth < 1634):
                     thalamus.append(neuron)
                 elif (clusterDepth > 1634 and clusterDepth < 2797):
                     hippocampus.append(neuron)
                 elif (clusterDepth > 2797 and clusterDepth < 3840):
                     visualCtx.append(neuron)
-                
-            accumulator += count
-    
 
-print ("Striatum (n = %d)" % len(striatum))
-print ("Motor Cortex (n = %d)" % len(motorCtx))
-print ("Thalamus (n = %d)" % len(thalamus))
-print ("Hippocampus (n = %d)" % len(hippocampus))
-print ("Visual Cortex (n = %d)" % len(visualCtx))
+            accumulator += count
+
+
+print("Striatum (n = %d)" % len(striatum))
+print("Motor Cortex (n = %d)" % len(motorCtx))
+print("Thalamus (n = %d)" % len(thalamus))
+print("Hippocampus (n = %d)" % len(hippocampus))
+print("Visual Cortex (n = %d)" % len(visualCtx))
 
 ########################################################
 # 2 Create Data Frame
@@ -162,7 +161,7 @@ df['start'] = np.squeeze(rawData['stimStarts'])
 df['stop'] = np.squeeze(rawData['stimStops'])
 df['stimulus'] = np.squeeze(rawData['stimIDs'])
 
-print (df.head())
+print(df.head())
 
 ########################################################
 # 3 Start Plotting
@@ -170,13 +169,16 @@ print (df.head())
 # 3.1 Striatum
 # ~~~~~~~~~~~~
 
-pop = PopVis(striatum, name='Striatum') 
+pop = PopVis(striatum, name='Striatum')
 
-fig = plt.figure(figsize=(30,20))
+fig = plt.figure(figsize=(30, 20))
 
-all_psth = pop.get_all_psth(event='start', df=df, conditions='stimulus', plot=False, binsize=100, window = [-500, 2000])
+all_psth = pop.get_all_psth(
+    event='start', df=df, conditions='stimulus', plot=False, binsize=100,
+    window=[-500, 2000])
 
-pop.plot_heat_map(all_psth, cond_id=[2, 7, 13], sortorder='descend', neuron_names=False) # cond_id chosen randomly
+pop.plot_heat_map(all_psth, cond_id=[
+                  2, 7, 13], sortorder='descend', neuron_names=False)
 
 ########################################################
 
@@ -186,13 +188,16 @@ pop.plot_population_psth(all_psth=all_psth, cond_id=[1, 7, 12])
 # 3.2 Frontal
 # ~~~~~~~~~~~~
 
-pop = PopVis(striatum + motorCtx, name='Frontal') 
+pop = PopVis(striatum + motorCtx, name='Frontal')
 
-fig = plt.figure(figsize=(30,20))
+fig = plt.figure(figsize=(30, 20))
 
-all_psth = pop.get_all_psth(event='start', df=df, conditions='stimulus', plot=False, binsize=100, window = [-500, 2000])
+all_psth = pop.get_all_psth(
+    event='start', df=df, conditions='stimulus', plot=False, binsize=100,
+    window=[-500, 2000])
 
-pop.plot_heat_map(all_psth, cond_id=[2, 7, 13], sortorder='descend', neuron_names=False)
+pop.plot_heat_map(
+    all_psth, cond_id=[2, 7, 13], sortorder='descend', neuron_names=False)
 
 ########################################################
 
@@ -202,13 +207,16 @@ pop.plot_population_psth(all_psth=all_psth, cond_id=[1, 7, 12])
 # 3.3 All Neurons
 # ~~~~~~~~~~~~
 
-pop = PopVis(striatum + motorCtx + thalamus + hippocampus + visualCtx) 
+pop = PopVis(striatum + motorCtx + thalamus + hippocampus + visualCtx)
 
-fig = plt.figure(figsize=(30,20))
+fig = plt.figure(figsize=(30, 20))
 
-all_psth = pop.get_all_psth(event='start', df=df, conditions='stimulus', plot=False, binsize=100, window = [-500, 2000])
+all_psth = pop.get_all_psth(
+    event='start', df=df, conditions='stimulus', plot=False, binsize=100,
+    window=[-500, 2000])
 
-pop.plot_heat_map(all_psth, cond_id=[2, 7, 13], sortorder='descend', neuron_names=False)
+pop.plot_heat_map(
+    all_psth, cond_id=[2, 7, 13], sortorder='descend', neuron_names=False)
 
 ########################################################
 
@@ -218,11 +226,15 @@ pop.plot_population_psth(all_psth=all_psth, cond_id=[1, 7, 12])
 # 3.4 Striatum vs. Motor Cortex
 # ~~~~~~~~~~~~
 
-striatumPop = PopVis(striatum, name='Striatum') 
-motorCtxPop = PopVis(motorCtx, name='Motor Cortex') 
+striatumPop = PopVis(striatum, name='Striatum')
+motorCtxPop = PopVis(motorCtx, name='Motor Cortex')
 
-striatum_psth = striatumPop.get_all_psth(event='start', df=df, conditions='stimulus', plot=False, binsize=100, window = [-500, 2000])
-motorCtx_psth = motorCtxPop.get_all_psth(event='start', df=df, conditions='stimulus', plot=False, binsize=100, window = [-500, 2000])
+striatum_psth = striatumPop.get_all_psth(
+    event='start', df=df, conditions='stimulus', plot=False, binsize=100,
+    window=[-500, 2000])
+motorCtx_psth = motorCtxPop.get_all_psth(
+    event='start', df=df, conditions='stimulus', plot=False, binsize=100,
+    window=[-500, 2000])
 
 ########################################################
 
@@ -231,5 +243,3 @@ striatumPop.plot_population_psth(all_psth=striatum_psth, cond_id=[1, 7, 12])
 ########################################################
 
 motorCtxPop.plot_population_psth(all_psth=motorCtx_psth, cond_id=[1, 7, 12])
-
-
